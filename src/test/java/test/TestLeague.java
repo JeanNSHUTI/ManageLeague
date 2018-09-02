75ea81b1757ecdf84fbd1ac13e6b6e3cf5e63e4a
JeanNSHUTI <16186@ecam.be>
2018-09-02 23:11:08 +0200

    tests

--- src/test/java/test/TestLeague.java
+++ src/test/java/test/TestLeague.java
@@ -238,7 +238,7 @@ public class TestLeague {
 
         assertTrue("No match was added", english_league.getList_of_matches().size() == result);
         assertThat("Date was not set",df.format(english_league.getList_of_matches().get(0).getMatch_date()), is(date));
-        assertThat("Home team not defined",english_league.getList_of_matches().get(0).getHome_team().getName(), is(home_team));
+        assertThat("Home team not defined",english_league.getList_of_matches().size(), is(result));
         assertThat("Away team not defined",english_league.getList_of_matches().get(0).getAway_team().getName(), is(away_team));
         english_league.reset();
     }
